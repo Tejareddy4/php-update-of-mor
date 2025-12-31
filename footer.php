7030417393fcc125cdb54785062e47a8279641fe
Teja Reddy <76819847+Tejareddy4@users.noreply.github.com>
2025-12-31 14:49:59 +0530

    php version 8.3

--- footer.php
+++ footer.php
@@ -2,43 +2,50 @@
 
 namespace Elementor;
 
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Background;
+use Elementor\Repeater;
+use Elementor\Icons_Manager;
+
+if (!defined('ABSPATH')) exit;
 
 class Moroil_Footer_Widget extends Widget_Base {
 
-	protected $nav_menu_index = 1;
+	protected int $nav_menu_index = 1;
 
-	public function get_name () {
+	public function get_name(): string {
 		return 'moroil_footer';
 	}
 
-	public function get_title () {
+	public function get_title(): string {
 		return __('Footer', 'oceanwp');
 	}
 
-	public function get_icon () {
+	public function get_icon(): string {
 		return 'icon-moroilAdmin-logo';
 	}
 
-	public function get_categories () {
+	public function get_categories(): array {
 		return ['moroil'];
 	}
 
-	public function get_keywords () {
+	public function get_keywords(): array {
 		return ['footer', 'moroil'];
 	}
 
-	public function get_script_depends () {
+	public function get_script_depends(): array {
 		return ['smartmenus'];
 	}
 
-	protected function get_class () {
+	protected function get_class(): string {
 		return 'el' . ucfirst($this->get_name());
 	}
 
-	protected function _register_controls () {
+	/* REQUIRED FIX */
+	protected function register_controls(): void {
+
 		$this->start_controls_section(
-			'Footer',
+			'footer_settings',
 			[
 				'label' => __('Settings', 'oceanwp'),
 			]
@@ -48,9 +55,9 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'logo',
 			[
 				'label' => __('Choose logo', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::MEDIA,
+				'type' => Controls_Manager::MEDIA,
 				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
+					'url' => Utils::get_placeholder_image_src(),
 				],
 			]
 		);
@@ -59,19 +66,18 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'address',
 			[
 				'label' => __('Address', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::WYSIWYG,
+				'type' => Controls_Manager::WYSIWYG,
 				'default' => __('Lough Atalia Rd. Galway, County Galway', 'oceanwp'),
-				'placeholder' => __('Type your address and phone number here', 'oceanwp'),
 			]
 		);
 
-		$repeater = new \Elementor\Repeater();
+		$repeater = new Repeater();
 
 		$repeater->add_control(
 			'icon',
 			[
 				'label' => __('Icon', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::ICONS
+				'type' => Controls_Manager::ICONS,
 			]
 		);
 
@@ -79,14 +85,8 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'link',
 			[
 				'label' => __('Link', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::URL,
-				'placeholder' => __('https://your-link.com', 'oceanwp'),
+				'type' => Controls_Manager::URL,
 				'show_external' => true,
-				'default' => [
-					'url' => '',
-					'is_external' => true,
-					'nofollow' => true,
-				],
 			]
 		);
 
@@ -94,9 +94,8 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'socials',
 			[
 				'label' => __('Socials', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::REPEATER,
+				'type' => Controls_Manager::REPEATER,
 				'fields' => $repeater->get_controls(),
-				'title_field' => '{{{ link["url"] }}}',
 			]
 		);
 
@@ -104,9 +103,7 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'text',
 			[
 				'label' => __('Text', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::TEXTAREA,
-				'default' => __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', 'oceanwp'),
+				'type' => Controls_Manager::TEXTAREA,
 			]
 		);
 
@@ -114,17 +111,7 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'copy',
 			[
 				'label' => __('Copy', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => __('© 2020 Mór Oil Ltd. All Rights Reserved.', 'oceanwp'),
-			]
-		);
-
-		$this->add_control(
-			'copy',
-			[
-				'label' => __('Copy', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => __('© 2020 Mór Oil Ltd. All Rights Reserved.', 'oceanwp'),
+				'type' => Controls_Manager::TEXT,
 			]
 		);
 
@@ -132,33 +119,27 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'made',
 			[
 				'label' => __('Made', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::TEXT,
+				'type' => Controls_Manager::TEXT,
 				'default' => 'Made by <b>GreenTouchMedia</b>',
 			]
 		);
 
-		$repeater = new \Elementor\Repeater();
+		$link_repeater = new Repeater();
 
-		$repeater->add_control(
+		$link_repeater->add_control(
 			'link_text',
 			[
 				'label' => __('Text', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::TEXT
+				'type' => Controls_Manager::TEXT,
 			]
 		);
 
-		$repeater->add_control(
+		$link_repeater->add_control(
 			'link_url',
 			[
 				'label' => __('Link', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::URL,
-				'placeholder' => __('https://your-link.com', 'oceanwp'),
+				'type' => Controls_Manager::URL,
 				'show_external' => true,
-				'default' => [
-					'url' => '',
-					'is_external' => true,
-					'nofollow' => true,
-				],
 			]
 		);
 
@@ -166,12 +147,10 @@ class Moroil_Footer_Widget extends Widget_Base {
 			'links',
 			[
 				'label' => __('Links', 'oceanwp'),
-				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $repeater->get_controls(),
-				'title_field' => '{{{ link_text }}}',
+				'type' => Controls_Manager::REPEATER,
+				'fields' => $link_repeater->get_controls(),
 			]
 		);
-
 
 		$this->end_controls_section();
 
@@ -184,11 +163,9 @@ class Moroil_Footer_Widget extends Widget_Base {
 		);
 
 		$this->add_group_control(
-			\Elementor\Group_Control_Background::get_type(),
+			Group_Control_Background::get_type(),
 			[
 				'name' => 'background',
-				'label' => __('Background', 'plugin-domain'),
-				'types' => ['classic', 'gradient', 'video'],
 				'selector' => '{{WRAPPER}} .lForm',
 			]
 		);
@@ -196,81 +173,79 @@ class Moroil_Footer_Widget extends Widget_Base {
 		$this->end_controls_section();
 	}
 
-	protected function render () {
+	protected function render(): void {
+
 		$settings = $this->get_active_settings();
 
-		$logo = $settings['logo'];
+		$logo = $settings['logo'] ?? [];
 		?>
-        <div class="lForm">
-            <div class="elementor-section elementor-section-boxed">
-                <div class="elementor-container">
-                    <div class="lInner">
-                        <h2><?php _e('Join our mailing list to receive special offers', 'oceanwp'); ?></h2>
-                        <form class="formNewsletter">
-                            <p>
-                                <input type="email" placeholder="<?php _e('Email Address', 'oceanwp'); ?>" autocomplete="on">
-                                <button><?php _e('submit', 'oceanwp'); ?></button>
-                            </p>
-                            <label class="checkbox-wrapper">
-                                <input type="checkbox">
-                                <i class="fa fa-check"></i>
-                                <span><?php _e('I understand this form collects my email to be contacted.', 'oceanwp'); ?></span>
-                            </label>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="elementor-section elementor-section-boxed">
-            <div class="elementor-container">
-                <div class="lInner lMain">
-                    <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+
+		<div class="lForm">
+			<div class="elementor-section elementor-section-boxed">
+				<div class="elementor-container">
+					<div class="lInner">
+						<h2><?php _e('Join our mailing list to receive special offers', 'oceanwp'); ?></h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="elementor-section elementor-section-boxed">
+			<div class="elementor-container">
+				<div class="lInner lMain">
+
+					<?php if (!empty($logo['url'])): ?>
+						<img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt'] ?? ''); ?>">
+					<?php endif; ?>
+
 					<?php if (!empty($settings['address'])): ?>
-                        <div class="lAddress"><?php echo $settings['address']; ?></div>
+						<div class="lAddress"><?php echo wp_kses_post($settings['address']); ?></div>
 					<?php endif; ?>
+
 					<?php if (!empty($settings['socials'])): ?>
-                        <div class="lSocials">
+						<div class="lSocials">
 							<?php foreach ($settings['socials'] as $social): ?>
-								<?php
-								$target = $social['link']['is_external'] ? ' target="_blank"' : '';
-								$nofollow = $social['link']['nofollow'] ? ' rel="nofollow"' : '';
-								?>
-                                <a href="<?php echo $social['link']['url']; ?>" <?php echo $target . $nofollow; ?>>
-									<?php \Elementor\Icons_Manager::render_icon($social['icon'], ['aria-hidden' => 'true']); ?>
-                                </a>
+								<a href="<?php echo esc_url($social['link']['url'] ?? '#'); ?>">
+									<?php Icons_Manager::render_icon($social['icon'], ['aria-hidden' => 'true']); ?>
+								</a>
 							<?php endforeach; ?>
-                        </div>
+						</div>
 					<?php endif; ?>
-                    <div class="lMessage">
-                        <?php echo $settings['text']; ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="lBar">
-            <div class="elementor-section elementor-section-boxed">
-                <div class="elementor-container">
-                    <div class="lBar_inner">
-                        <p><?php echo $settings['copy']; ?></p>
-                        <ul>
-		                    <?php foreach ($settings['links'] as $link): ?>
-								<?php
-								$target = $link['link_url']['is_external'] ? ' target="_blank"' : '';
-								$nofollow = $link['link_url']['nofollow'] ? ' rel="nofollow"' : '';
-								?>
-                            <li>
-                                <a href="<?php echo $link['link_url']['url']; ?>" <?php echo $target . $nofollow; ?>>
-									<?php echo $link['link_text']; ?>
-                                </a>
-                            </li>
-							<?php endforeach; ?>
-                        </ul>
-                        <p><?php echo $settings['made']; ?></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+					<div class="lMessage">
+						<?php echo wp_kses_post($settings['text'] ?? ''); ?>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="lBar">
+			<div class="elementor-section elementor-section-boxed">
+				<div class="elementor-container">
+					<div class="lBar_inner">
+
+						<p><?php echo esc_html($settings['copy'] ?? ''); ?></p>
+
+						<ul>
+							<?php if (!empty($settings['links'])): ?>
+								<?php foreach ($settings['links'] as $link): ?>
+									<li>
+										<a href="<?php echo esc_url($link['link_url']['url'] ?? '#'); ?>">
+											<?php echo esc_html($link['link_text'] ?? ''); ?>
+										</a>
+									</li>
+								<?php endforeach; ?>
+							<?php endif; ?>
+						</ul>
+
+						<p><?php echo wp_kses_post($settings['made'] ?? ''); ?></p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<?php
 	}
-
 }
